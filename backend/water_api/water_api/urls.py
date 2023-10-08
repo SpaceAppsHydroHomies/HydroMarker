f588b77ebd13570d507fbbcea432c379deca663f
Ben Serrano <b.serrano8970@gmail.com>
2023-10-07 20:34:44 -0500

    Add first implementation of get_data endpoint
    Added water_quality package, did parsing to ensure the lat and long received are both ints or floats.

--- backend/water_api/water_api/urls.py
+++ backend/water_api/water_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("animals/", include("animals.urls")),
+    path("water_quality/", include("water_quality.urls"))
 ]
---
+++ backend/water_api/water_quality/views.py
@@ -0,0 +1,19 @@
+from django.http import JsonResponse
+
+
+def index(response):
+    return JsonResponse({'test': 'data'})
+
+
+def get_data(request):
+    if request.method == "GET":
+        lat, long = request.GET.get('lat'), request.GET.get('long')
+        if not isinstance(lat, (float, int)) or not isinstance(long, (float, int)):
+            return JsonResponse({'status': 400, 'reason': 'lat or long was not an int or float'},
+                                status=400)
+        if lat > 180 or lat < -180 or long < -180 or long > 180:
+            return JsonResponse({'status': 400, 'reason': 'latitude and longitude should be between -180 and 180'})
+        # TODO: Get the HUC of the closest body of water
+        # TODO: Return the data for that HUC
+        # TODO: Get the colloquial name of the body of water we're in
+        return JsonResponse({'lat': lat, 'long': long})
