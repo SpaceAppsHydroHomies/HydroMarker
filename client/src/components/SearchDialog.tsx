248c0a413ee7a1fcd204f6bfc87ec5fe2f950d37
Minaira <nicholasang2626@gmail.com>
2023-10-08 10:24:44 -0500

    Fix fetch for search page and endangered slider

--- client/src/components/SearchDialog.tsx
+++ client/src/components/SearchDialog.tsx
@@ -1,35 +1,58 @@
 import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
+    Command,
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
 } from "@/components/ui/command";
+import { useEffect, useState } from "react";
 
-function SearchDialog({
-  open,
-  onClose,
+function SearchDialog ({
+    open,
+    onClose,
 }: {
-  open: boolean;
-  onClose: () => void;
+    open: boolean;
+    onClose: () => void;
 }) {
-  return (
-    <CommandDialog open={open} onOpenChange={onClose}>
-      <CommandInput placeholder="Search for a waterbody" />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions">
-          <CommandItem>water1</CommandItem>
-          <CommandItem>water2</CommandItem>
-          <CommandItem>water1</CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-      </CommandList>
-    </CommandDialog>
-  );
+    const [searchResults, setSearchResults] = useState<
+        searchResultsProps[]
+    >([]);
+    useEffect(() => {
+        (async () => {
+            // Define the URL for the API endpoint
+            const apiUrl =
+                "http://127.0.0.1:8000/animals/get_ecosystems/";
+            try {
+                // Fetch data from the API using the fetch() function
+                const data = await fetch(apiUrl)
+                    .then((response) => response.json())
+                    .then((responseData) => {
+                        // Set the state variable using the data returned from the API
+                        setSearchResults(responseData["Ecosystems"]);
+                    });
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        })();
+    }, []);
+    return (
+        <CommandDialog open={open} onOpenChange={onClose}>
+            <CommandInput placeholder="Search for a body of water" />
+            <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="Suggestions">
+                    {searchResults.map((results, key) => (
+                        <CommandItem key={key}>{results}</CommandItem>
+                    ))}
+                    <CommandItem>sdsd</CommandItem>
+                </CommandGroup>
+                <CommandSeparator />
+            </CommandList>
+        </CommandDialog>
+    );
 }
 
 export default SearchDialog;
