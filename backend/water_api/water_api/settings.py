7ab03a5b81bc44e998d45dcbba253cf6562d2757
Minaira <nicholasang2626@gmail.com>
2023-10-08 06:43:40 -0500

    Fix CORS error from Vite Frontend

--- backend/water_api/water_api/settings.py
+++ backend/water_api/water_api/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "animals",
     "water_quality",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,3 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ["http://localhost:5173"]
--- client/src/components/EndangeredSlider.tsx
+++ client/src/components/EndangeredSlider.tsx
@@ -1,26 +1,43 @@
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { useEffect, useState } from "react"
 
 const images = ["cat1.jpg", "cat2.jpg", "cat3.jpg", "cat4.jpg"];
 
 const EndangeredSlider = () => {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Endangered Species</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="flex overflow-x-auto">
-          {images.map((image) => (
-            <img
-              className="w-52 h-52 rounded-xl object-cover"
-              src={`/${image}`}
-              alt="cat"
-            />
-          ))}
-        </div>
-      </CardContent>
-    </Card>
-  );
+
+    // Effect to fetch data when the component mounts
+    useEffect(() => {
+        // Define the URL for the API endpoint
+        const apiUrl = 'http://127.0.0.1:8000/animals/get_endangered_species_data/Gulf%20of%20Mexico/';
+
+        // Fetch data from the API using the fetch() function
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(responseData)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Endangered Species</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="flex overflow-x-auto">
+                    {images.map((image) => (
+                        <img
+                            className="w-52 h-52 rounded-xl object-cover"
+                            src={`/${image}`}
+                            alt="cat"
+                        />
+                    ))}
+                </div>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default EndangeredSlider;
